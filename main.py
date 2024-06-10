cf02dd1b8935408f11943c067334d2b505ec59ad
Linsent20 <121661256+Linsent20@users.noreply.github.com>
2024-06-10 11:52:59 +0800

    随机调整签到时间，减小官方封号概率

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0'
         }
         bbsid = re.findall(r'oi=[0-9]+', token)[0].replace('oi=', '')
-        wait_time = random.randint(10,200 ) # Random Sleep to Avoid Ban
+        wait_time = random.randint(1,2500) # Random Sleep to Avoid Ban
         print(f'为了避免同一时间签到人数太多导致被官方怀疑，开始休眠 {wait_time} 秒')
         time.sleep(wait_time)
         wallet = r.get(WalletURL, headers=headers, timeout=60)
